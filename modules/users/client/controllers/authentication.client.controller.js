68da3fd86bff3d756e4d2614d0611a6fe562f492
Alex Heuser <aheuser@ufl.edu>
2017-10-05 00:20:03 -0400

    Added permission check on login, admin gets directed to admin views, user to user views

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -47,7 +47,11 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$stat
         $scope.authentication.user = response;
 
         // And redirect to the previous or home page
-        $state.go(/*$state.previous.state.name || */'mainmenu'/*, $state.previous.params*/);
+        if ("admin" === Authentication.user.roles[0]) {
+          $state.go('mainmenuadmin');
+        } else {
+          $state.go('mainmenu');
+        }
       }).error(function (response) {
         $scope.error = response.message;
       });
