93bb2699b3b711fc5e7ef257a4632edf08109524
eristow <evan.ristow1@gmail.com>
2017-11-02 03:32:29 -0400

    fixed some user problems. create now works. other things

--- modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
+++ modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
@@ -2,19 +2,50 @@
   'use strict';
 
   angular
-    .module('inventorymanagements')
-    .controller('InventorymanagementsReceiveController', InventorymanagementsReceiveController);
+  .module('inventorymanagements')
+  .controller('InventorymanagementsReceiveController', InventorymanagementsReceiveController);
 
-  InventorymanagementsReceiveController.$inject = ['InventorymanagementsService'];
+  InventorymanagementsReceiveController.$inject = ['InventorymanagementsService', '$scope', '$state'];
 
-  function InventorymanagementsReceiveController(InventorymanagementsService) {
+  function InventorymanagementsReceiveController(InventorymanagementsService, $scope, $state) {
     var vm = this;
-
+    $scope.state = $state;
     vm.inventorymanagements = InventorymanagementsService.query();
 
-    function receive(upc, quantity) {
+    $scope.receive = function() {
       //search for UPC in DB. if there, add quantity. if not, send to create page.
-      //vm.inventorymanagement.quantity += quantity;
-    }
+
+      //TODO: remove these
+      console.log($scope.upc.upc);
+      console.log($scope.quantity);
+
+      var invResult = -1;
+      for (var i = 0; i < vm.inventorymanagements.length; i++) {
+        if (vm.inventorymanagements[i].upc === $scope.upc.upc) {
+          invResult = i;
+          break;
+        }
+      }
+
+      if(invResult === -1) {
+        $state.go();
+      }
+      else {
+        var quan = parseInt($scope.quantity);
+        vm.inventorymanagements[invResult].qty += quan;
+
+        vm.inventorymanagements[invResult].$update(successCallback, errorCallback);
+      }
+
+      function successCallback(res) {
+        //TODO: should this route to same page, or to list?
+        //TODO: also needs to pop-up a success message
+        $state.go('inventorymanagements.receive');
+      }
+
+      function errorCallback(res) {
+        vm.error = res.data.message;
+      }
+    };
   }
 }());
