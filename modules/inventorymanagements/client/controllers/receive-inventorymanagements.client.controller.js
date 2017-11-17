8b332c9b2da807ffb62fbd0b5b65fd6298a3167f
eristow <evan.ristow1@gmail.com>
2017-11-16 20:50:07 -0500

    Updated receive inv view. Just needs passing upc to create inv view.

--- modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
+++ modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
@@ -12,34 +12,54 @@
     $scope.state = $state;
     vm.inventorymanagements = InventorymanagementsService.query();
 
+    function toasty() {
+      var x = document.getElementById("snackbar");
+      x.className = "show";
+      setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    }
+
     $scope.receive = function() {
       //search for UPC in DB. if there, add quantity. if not, send to create page.
-
-      //TODO: remove these
-      console.log($scope.upc.upc);
-      console.log($scope.quantity);
-
       var invResult = -1;
-      for (var i = 0; i < vm.inventorymanagements.length; i++) {
-        if (vm.inventorymanagements[i].upc === $scope.upc.upc) {
-          invResult = i;
-          break;
+      if (!$scope.upc.upc && !$scope.quantity) {
+        alert("You must fill in Quantity and UPC first");
+      } else if (!$scope.upc.upc) {
+        alert("You must fill in UPC first");
+      } else if (!$scope.quantity) {
+        alert("You must fill in Quantity first");
+      } else {
+        if($scope.quantity <= 0) {
+          alert("Quantity must be greater than 0")
+        }
+        else {
+          for (var i = 0; i < vm.inventorymanagements.length; i++) {
+            if (vm.inventorymanagements[i].upc === $scope.upc.upc) {
+              invResult = i;
+              break;
+            }
+          }
+          // if upc isn't in database, go to create view
+          if(invResult === -1) {
+            //TODO: figure out stateParams for create view
+            $state.go('inventorymanagements.create');//, $state.upc.upc);
+          }
+          else {
+            var quan = parseInt($scope.quantity);
+            vm.inventorymanagements[invResult].qty += quan;
+
+            vm.inventorymanagements[invResult].$update(successCallback, errorCallback);
+          }
         }
       }
 
-      if(invResult === -1) {
-        $state.go();
-      }
-      else {
-        var quan = parseInt($scope.quantity);
-        vm.inventorymanagements[invResult].qty += quan;
-
-        vm.inventorymanagements[invResult].$update(successCallback, errorCallback);
-      }
 
       function successCallback(res) {
-        //TODO: should this route to same page, or to list?
         //TODO: also needs to pop-up a success message
+        // toast
+        toasty();
+        // reset quantity field
+        $scope.quantity = null;
+        // reload page
         $state.go('inventorymanagements.receive');
       }
 
