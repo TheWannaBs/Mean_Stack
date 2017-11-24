b8b141901386e339683fc47cacc3690956d258c3
eristow <evan.ristow1@gmail.com>
2017-11-24 13:34:38 -0500

    added multi-upc/quantity to receieve view. looks ugly, and not merged with my other receive changes yet.

--- modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
+++ modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
@@ -11,30 +11,39 @@
     var vm = this;
     $scope.state = $state;
     vm.inventorymanagements = InventorymanagementsService.query();
+    $scope.choices = [{ id: 'choice1' }, { id: 'choice2' }];
+
+    $scope.addNewChoice = function() {
+      var newItemNo = $scope.choices.length+1;
+      $scope.choices.push({ 'id':'choice'+newItemNo });
+    };
+
+    $scope.removeChoice = function() {
+      var lastItem = $scope.choices.length-1;
+      $scope.choices.splice(lastItem);
+    };
 
     $scope.receive = function() {
       //search for UPC in DB. if there, add quantity. if not, send to create page.
 
-      //TODO: remove these
-      console.log($scope.upc.upc);
-      console.log($scope.quantity);
-
-      var invResult = -1;
-      for (var i = 0; i < vm.inventorymanagements.length; i++) {
-        if (vm.inventorymanagements[i].upc === $scope.upc.upc) {
-          invResult = i;
-          break;
+      for (var i = 0; i < $scope.choices.length; i++) {
+        var invResult = -1;
+        for (var j = 0; j < vm.inventorymanagements.length; j++) {
+          if (vm.inventorymanagements[j].upc === $scope.choices[i].upc.upc) {
+            invResult = j;
+            break;
+          }
         }
-      }
 
-      if(invResult === -1) {
-        $state.go();
-      }
-      else {
-        var quan = parseInt($scope.quantity);
-        vm.inventorymanagements[invResult].qty += quan;
+        if(invResult === -1) {
+          $state.go();
+        }
+        else {
+          var quan = parseInt($scope.choices[i].quantity);
+          vm.inventorymanagements[invResult].qty += quan;
 
-        vm.inventorymanagements[invResult].$update(successCallback, errorCallback);
+          vm.inventorymanagements[invResult].$update(successCallback, errorCallback);
+        }
       }
 
       function successCallback(res) {
