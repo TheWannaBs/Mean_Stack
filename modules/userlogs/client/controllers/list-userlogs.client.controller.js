e8ac4bc679d9d0be549aeb0b5d280c202d718ca9
Dylan White <dkwhite200@ufl.edu>
2017-11-26 22:47:04 -0500

    userlog list view sorts better

--- modules/userlogs/client/controllers/list-userlogs.client.controller.js
+++ modules/userlogs/client/controllers/list-userlogs.client.controller.js
@@ -11,9 +11,32 @@
     var vm = this;
 
     vm.userlogs = UserlogsService.query();
-    vm.headsort = "timestamp";
+    vm.headSort = 'timestamp';
+    vm.reverse = true;
+    vm.changeSort = changeSort;
 
-    /* THIS IS FOR PAGENATION DO NOT DELETE
+    function changeSort(headName) {
+      vm.headSort = headName;
+      vm.reverse = (headName === vm.headSort) ? !vm.reverse : false;
+    }
+
+    /*
+    function reverseSort() {
+      var len = vm.headsort.length;
+      if (vm.headsort.substring(len - 4, len) == 'true') {
+        vm.headsort = vm.headsort.replaceAt(len - 4, 'false');
+      }
+      else {
+        vm.headsort = vm.headsort.replaceAt(len - 4, 'false');
+      }
+    }
+
+    //this is a helper function to replace strings at specific indicies with a substring 
+    String.prototype.replaceAt = function (index, replacement) {
+      return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+    }
+
+    THIS IS FOR PAGENATION DO NOT DELETE
     vm.buildPager = function () {
       vm.pagedItems = [];
       vm.itemsPerPage = 20;
