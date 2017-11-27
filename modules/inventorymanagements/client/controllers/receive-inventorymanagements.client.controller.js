b2c29fb6ed54de0b34fa05da6f935667504f45e9
eristow <evan.ristow1@gmail.com>
2017-11-27 14:14:42 -0500

    quagga works, but user cannot select which upc field to scan into

--- modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
+++ modules/inventorymanagements/client/controllers/receive-inventorymanagements.client.controller.js
@@ -11,11 +11,109 @@
     var vm = this;
     $scope.state = $state;
     vm.inventorymanagements = InventorymanagementsService.query();
-    $scope.choices = [{ id: 'choice1', upc: '', quantity: '' }];
+    $scope.choices = [{ id: 'choice1', upc: {}, quantity: '' }];
+
+    var _scannerIsRunning = false;
+    var scanThis = 0;
+
+    function startScanner() {
+      Quagga.init({
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          target: document.querySelector('#scanner-container'),
+          constraints: {
+            width: 480,
+            height: 320,
+            facingMode: 'environment'
+          },
+        },
+        decoder: {
+          readers: [
+            'code_128_reader',
+            'ean_reader',
+            'ean_8_reader',
+            'code_39_reader',
+            'code_39_vin_reader',
+            'codabar_reader',
+            'upc_reader',
+            'upc_e_reader',
+            'i2of5_reader'
+          ],
+          debug: {
+            showCanvas: false,
+            showPatches: true,
+            showFoundPatches: true,
+            showSkeleton: true,
+            showLabels: true,
+            showPatchLabels: true,
+            showRemainingPatchLabels: true,
+            boxFromPatches: {
+              showTransformed: true,
+              showTransformedBox: true,
+              showBB: true
+            }
+          }
+        },
+
+      }, function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        console.log('Initialization finished. Ready to start');
+        Quagga.start();
+
+        // Set flag to is running
+        _scannerIsRunning = true;
+      });
+
+      Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+          drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+          if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute('width')), parseInt(drawingCanvas.getAttribute('height')));
+            result.boxes.filter(function (box) {
+              return box !== result.box;
+            }).forEach(function (box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: 'green', lineWidth: 2 });
+            });
+          }
+
+          if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 });
+          }
+
+          if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+          }
+        }
+      });
+
+      Quagga.onDetected(function (result) {
+        console.log('Barcode detected and processed : [' + result.codeResult.code + ']', result);
+        $scope.choices[scanThis].upc.upc = result.codeResult.code;
+        console.log($scope.choices[scanThis].upc.upc);
+        $state.go('inventorymanagements.receive');
+      });
+    }
+
+    //Start/stop scanner
+    document.getElementById('btn').addEventListener('click', function () {
+      if (_scannerIsRunning) {
+        Quagga.stop();
+      } else {
+        startScanner();
+      }
+    }, false);
 
     $scope.addNewChoice = function() {
       var newItemNo = $scope.choices.length+1;
-      $scope.choices.push({ 'id':'choice'+newItemNo, upc: '', quantity: '' });
+      $scope.choices.push({ 'id':'choice'+newItemNo, upc: {}, quantity: '' });
+      scanThis++;
     };
 
     $scope.removeChoice = function() {
