272a61843373239abf6a9ceb40220b0a239cbd8d
Alex Heuser <aheuser@ufl.edu>
2017-11-29 19:36:24 -0500

    First implementation of quagga on move view

--- modules/clientmanagements/client/controllers/clientinventory.client.controller.js
+++ modules/clientmanagements/client/controllers/clientinventory.client.controller.js
@@ -85,6 +85,109 @@
       }
     };
 
+    var _scannerIsRunning = false;
+    var _firstRun = true;
+    //on-click for start/stop scanner button
+    function startScanner() {
+      Quagga.init({
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          constraints: {
+            width: 480,
+            height: 320,
+            facingMode: 'environment'
+          },
+        },
+        decoder: {
+          readers: [
+            'code_128_reader',
+            'ean_reader',
+            'ean_8_reader',
+            'code_39_reader',
+            'code_39_vin_reader',
+            'codabar_reader',
+            'upc_reader',
+            'upc_e_reader',
+            'i2of5_reader'
+          ],
+          debug: {
+            showCanvas: true,
+            showPatches: true,
+            showFoundPatches: true,
+            showSkeleton: true,
+            showLabels: true,
+            showPatchLabels: true,
+            showRemainingPatchLabels: true,
+            boxFromPatches: {
+              showTransformed: true,
+              showTransformedBox: true,
+              showBB: true
+            }
+          }
+        },
+
+      }, function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+
+        console.log('Initialization finished. Ready to start');
+        Quagga.start();
+
+        // Set flag to "is running"
+        _scannerIsRunning = true;
+        _firstRun = false;
+      });
+
+      //When barcode is processed. draws rectangle around barcode
+      //TODO: fix the drawing below camera issue
+      Quagga.onProcessed(function (result) {
+        var drawingCtx = Quagga.canvas.ctx.overlay,
+          drawingCanvas = Quagga.canvas.dom.overlay;
+
+        if (result) {
+          if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute('width')), parseInt(drawingCanvas.getAttribute('height')));
+            result.boxes.filter(function (box) {
+              return box !== result.box;
+            }).forEach(function (box) {
+              Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: 'green', lineWidth: 2 });
+            });
+          }
+
+          if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: '#00F', lineWidth: 2 });
+          }
+
+          if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+          }
+        }
+      });
+
+      //When barcode is detected. Puts barcode code into upc box
+      Quagga.onDetected(function (result) {
+        console.log('Barcode detected and processed : [' + result.codeResult.code + ']', result);
+        $scope.serial[upcFields-1].upc = result.codeResult.code;
+        Quagga.stop();
+      });
+    }
+
+    //Start/stop scanner
+    //TODO: check the restart functionality
+    document.getElementById('startbtn').addEventListener('click', function () {
+      if (_firstRun) {
+        startScanner();
+      } else if (_scannerIsRunning) {
+        Quagga.stop();
+      } else {
+        Quagga.start();
+      }
+      // console.log(_scannerIsRunning);
+    }, false);
+
     // checks validity of all upcs/quantities/clients and then moves items from the inventory to the clients
     $scope.moveToClient = function () {
       // first loop through to verify the fields are valid
@@ -197,9 +300,9 @@
       }
       if (alerts.length !== 0) {
         // throw the alerts
-        alert(alerts);  
+        alert(alerts);
       }
-      
+
       if (skipUPC.length !== upcFields) {
         // gimme that toast
         toasty();
